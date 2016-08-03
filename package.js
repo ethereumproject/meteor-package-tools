36b9252af9cb10cd14e5822dbedb6188b0d725b6
gravity <your_email@example.com>
2016-08-03 08:08:58 +0200

    Changing the meteor package namespace

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'ethereum:tools',
+  name: 'etc:tools',
   summary: 'Helper functions for dapps',
   version: '0.5.2',
   git: 'http://github.com/ethereumproject/meteor-package-tools'
@@ -10,25 +10,18 @@ Package.onUse(function(api) {
   api.use('underscore', ['client', 'server']);
   api.use('mongo', ['client', 'server']);
   api.use('http', ['client', 'server']);
-
   api.use('spacebars', 'client');
   api.use('templating', 'client');
   api.use('tracker', 'client');
-
   //api.use('numeral:numeral@1.5.3', ['client', 'server']);
   api.use('3stack:bignumber@2.0.0', 'client');
-
-  // !EPROJECT Restorted web3 will be required
-  api.use('ethereum:web3@0.15.1', ['client', 'server']);
+  // !EPROJECT Restored web3 will be required
+  api.use('etc:web3@0.15.1', ['client', 'server']);
   api.use('frozeman:persistent-minimongo@0.1.8', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
-
   api.export(['EthTools'], ['client', 'server']);
-
-
   api.addFiles('ethtools.js', ['client', 'server']);
   api.addFiles('ticker.js', ['client', 'server']);
-
   api.addFiles('globalHelpers.js', 'client');
 });
 
@@ -37,4 +30,3 @@ Package.onTest(function(api) {
   api.use('ethereum:tools');
   api.addFiles('ethtools-tests.js', ['client', 'server']);
 });
-
